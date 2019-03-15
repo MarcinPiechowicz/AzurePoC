5aedb2f69737d981ab1f129e344dccfa079a9071
Marcin Piechowicz <info@marcinp.de>
2019-03-15 14:32:43 +0100

    Added storeEvent RESTful interface

---
+++ src/main/java/com/sap/corp/azurepoc/EventController.java
@@ -0,0 +1,23 @@
+package com.sap.corp.azurepoc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+
+    @RequestMapping("/storeEvent")
+    public String storeEvent(@RequestParam(value = "departure") String eventType) {
+
+        /*
+        Event Type 1 = departure
+        Event Type 2 = arrival
+
+        Business Partner 1 = Bosch
+        Business Partner 2 = Daimler
+         */
+        return "eventType " + eventType;
+    }
+
+}
